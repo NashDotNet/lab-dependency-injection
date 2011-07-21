73cb7b979ea489752c1068a57df43f9bc4cfd45d
Bryan Hunter <bryan.hunter@fireflylogic.com>
2011-07-21 11:30:47 -0500

    Added Castle samples

---
+++ src/Castle/CastleWithXmlAndCodeRegistration/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Castle.Core.Resource;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.Configuration.Interpreters;
+
+namespace CastleWithXmlAndCodeRegistration
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Note: look at app.config and see what IMenu registrations are being wired up there.
+
+            // Our strategy here is to set the defaults in code, and let config override them.
+            IWindsorContainer container = new WindsorContainer(new XmlInterpreter(new ConfigResource("castle")));
+
+            container.Register(Component.For<IWaiter>().ImplementedBy<Waiter>().Named("waiter"));
+            container.Register(Component.For<IMenu>().ImplementedBy<LunchMenu>().Named("menu"));
+
+            var waiter = container.Resolve<IWaiter>();
+
+            Console.WriteLine(waiter.AnnounceTheSpecials());
+            Console.ReadKey();
+        }
+    }
+
+    public interface IMenu
+    {
+        string TheSpecials { get; }
+    }
+
+    public interface IWaiter
+    {
+        string AnnounceTheSpecials();
+    }
+
+    public class Waiter : IWaiter
+    {
+        private readonly IMenu _menu;
+
+        public Waiter(IMenu menu)
+        {
+            _menu = menu;
+        }
+
+        public string AnnounceTheSpecials()
+        {
+            return "May I suggest the " + _menu.TheSpecials;
+        }
+    }
+
+    public class LunchMenu : IMenu
+    {
+        public string TheSpecials
+        {
+            get { return "ham and cheese with a frosty root beer"; }
+        }
+    }
+
+    public class BreakfastMenu : IMenu
+    {
+        public string TheSpecials
+        {
+            get { return "bacon and eggs with strong coffee"; }
+        }
+    }
+}
---
+++ src/Castle/SimplestThing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+
+namespace SimplestThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+            container.Register(Component.For<IPet>().ImplementedBy<Cat>().Named("cat"));
+            container.Register(Component.For<IPet>().ImplementedBy<Dog>().Named("dog"));
+
+            var defaultPet = container.Resolve<IPet>();
+            var cat = container.Resolve<IPet>("cat");
+            var dog = container.Resolve<IPet>("dog");
+
+            Console.WriteLine("Default IPet says " + defaultPet.Speak());
+
+            Console.WriteLine("IPet named [cat] says " + cat.Speak());
+            Console.WriteLine("IPet named [dog] says " + dog.Speak());
+
+            Console.ReadKey();
+        }
+
+        public interface IPet
+        {
+            string Speak();
+        }
+
+        public class Dog : IPet
+        {
+            public string Speak()
+            {
+                return "Woof";
+            }
+        }
+
+        public class Cat : IPet
+        {
+            public string Speak()
+            {
+                return "Meow";
+            }
+        }
+    }
+}
