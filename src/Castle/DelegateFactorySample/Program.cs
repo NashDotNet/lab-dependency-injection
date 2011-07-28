20305a8ed7b00a57e423b357c839a1c32d469e9a
Bryan Hunter <bryan.hunter@fireflylogic.com>
2011-07-28 17:50:11 -0500

    Added DelegateFactorySample and cleaned up the performace sample

---
+++ src/Castle/DelegateFactorySample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Castle.Facilities.TypedFactory;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+
+namespace DelgateFactorySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+
+            container.AddFacility<TypedFactoryFacility>();
+            container.Register(Component.For<IBaker>().ImplementedBy<Baker>());
+            container.Register(Component.For<ICake>().ImplementedBy<Cake>().LifeStyle.Transient);
+
+            var baker = container.Resolve<IBaker>();
+
+            Console.WriteLine(baker.BakeCake());
+            Console.WriteLine(baker.BakeCake());
+            Console.WriteLine(baker.BakeCake());
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IBaker
+    {
+        ICake BakeCake();
+    }
+
+    public class Baker : IBaker
+    {
+        private readonly Func<ICake> _cakeMaker;
+
+        public Baker(Func<ICake> cakeMaker)
+        {
+            _cakeMaker = cakeMaker;
+        }
+
+        public ICake BakeCake()
+        {
+            return _cakeMaker();
+        }
+    }
+
+    public class Cake : ICake
+    {
+        public override string ToString()
+        {
+            return String.Format("Delicious Cake (serial# {0})", this.GetHashCode());
+        }
+    }
+
+    public interface ICake
+    {
+    }
+}
--- src/Castle/PerformanceSample/Program.cs
+++ src/Castle/PerformanceSample/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Castle.Facilities.TypedFactory;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
@@ -14,62 +15,69 @@ namespace PerformanceSample
         {
             var ticks = DateTime.Now.Ticks;
 
-            Console.WriteLine("Start : " + new TimeSpan((DateTime.Now.Ticks - ticks)));
+            Console.WriteLine("Start : {0}", GetTimeElapsedSince(ticks));
 
             var container = new WindsorContainer();
-            Console.WriteLine("Container created : " + new TimeSpan( (DateTime.Now.Ticks - ticks)));
-            
+            Console.WriteLine("Container created : {0}", GetTimeElapsedSince(ticks));
+
             container.Install(
-                //new DummyInstaller(), 
+                new DummyInstaller(),
                 new NestedInstaller());
-            Console.WriteLine("Types registered : " +  new TimeSpan((DateTime.Now.Ticks - ticks)));
-            
-            //var dummy = container.Resolve<IDummy100Service>();
-            //Console.WriteLine(dummy);
-            //Console.WriteLine("Resolved IDummy100Service : " +  new TimeSpan((DateTime.Now.Ticks - ticks)));
-            
+
+            Console.WriteLine("Types registered [{1}] : {0}", GetTimeElapsedSince(ticks), 1);
+
+            var dummy = container.Resolve<IDummy100Service>();
+            Console.WriteLine(dummy);
+            Console.WriteLine("Resolved IDummy100Service : {0}", GetTimeElapsedSince(ticks));
+
             var nested = container.Resolve<INested0Service>();
             Console.WriteLine(nested);
-            Console.WriteLine("Resolved INested0Service : " + new TimeSpan((DateTime.Now.Ticks - ticks)));
+            Console.WriteLine("Resolved INested0Service : {0}", GetTimeElapsedSince(ticks));
 
             Console.ReadKey();
         }
+
+        private static TimeSpan GetTimeElapsedSince(long previousTicks)
+        {
+            return new TimeSpan((DateTime.Now.Ticks - previousTicks));
+        }
     }
-        public class DummyInstaller : IWindsorInstaller
+
+    public class DummyInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            public void Install(IWindsorContainer container, IConfigurationStore store)
-            {
-                container.Register(AllTypes.FromThisAssembly()
-                                       .Where(x => x.Name.StartsWith("Dummy"))
-                                       .WithService.FirstInterface()
-                                       .Configure(c => c.LifeStyle.Transient)
-                    );
-            }
+            container.Register(AllTypes.FromThisAssembly()
+                                   .Where(x => x.Name.StartsWith("Dummy"))
+                                   .WithService.FirstInterface()
+                                   .Configure(c => c.LifeStyle.Transient)
+                );
         }
+    }
 
-        public class NestedInstaller : IWindsorInstaller
+    public class NestedInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            public void Install(IWindsorContainer container, IConfigurationStore store)
-            {
-                container.Register(AllTypes.FromThisAssembly()
-                                       .Where(x => x.Name.StartsWith("Nested"))
-                                       .WithService.FirstInterface()
-                                       .Configure(c => c.LifeStyle.Singleton)
-                    );
-            }
+            container.Register(AllTypes.FromThisAssembly()
+                                   .Where(x => x.Name.StartsWith("Nested"))
+                                   .WithService.FirstInterface()
+                                   .Configure(c => c.LifeStyle.Singleton)
+                );
         }
+    }
 
-        //var sb = new StringBuilder();
+    //var sb = new StringBuilder();
 
-        //for(var x = 0; x < 1000; x++)
-        //{  
-        //    var name = "Nested" + x + "Service";
+    //for(var x = 0; x < 1000; x++)
+    //{  
+    //    var name = "Nested" + x + "Service";
 
-        //    sb.AppendFormat("public interface I{0} {{}}\r\n", name);
-        //    sb.AppendFormat("public class {0} : I{0} {{\r\npublic {0}({1} dep){{}}\r\n}}\r\n", 
-        //        name, 
-        //        "Nested" + (x+1) + "Service");
-        //}
+    //    sb.AppendFormat("public interface I{0} {{}}\r\n", name);
+    //    sb.AppendFormat("public class {0} : I{0} {{\r\npublic {0}({1} dep){{}}\r\n}}\r\n", 
+    //        name, 
+    //        "Nested" + (x+1) + "Service");
+    //}
 
-        //Console.WriteLine(sb.ToString());
+    //Console.WriteLine(sb.ToString());
 }
